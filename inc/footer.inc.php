802403d8f0f2ba9f680d7bc8e4a5c700eb961801
nelsonvan <nelson.vanicatte@lepoles.com>
2019-07-18 09:00:16 +0200

    first commit

---
+++ inc/footer.inc.php
@@ -0,0 +1,16 @@
+
+    <!-- Lien vers mon fichier CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<div class="footer">
+        <footer class="bg-info text-white text-center p-1 fixed-bottom">
+            &copy; 2019 - GL - Pas touche c'est mon site a moi !!
+            <a class="offset-md-1 d-block mt-2" href="#">1</a>
+        </footer>
+        <!-- CDN Javscript bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <!-- Lien js perso -->
+        <script src ="../js/index.js"></script>
+    </div>
---
+++ inc/header.inc.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Lien vers mon fichier CSS -->
+    <link rel="stylesheet" href="css/style.css">
+     <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-C++cugH8+Uf86JbNOnQoBweHHAe/wVKN/mb0lTybu/NZ9sEYbd+BbbYtNpWYAsNP" crossorigin="anonymous">
+    
+    
+    <title>SweeperFootball</title>
+</head>
+<body>
+  
+</body>
+<div class="container">
+    <h1 class="mt-5 text-center text-success ml-2">SweeperFootball</h1>
+   <img class="offset-md-6" src="img/ballon3.gif" href="">
+    </div>
+   <div class="container-fluid">
+       <header>
+       <nav class="navbar navbar-expand-lg navbar-info bg-dark mt-4">
+  <a class="navbar-brand text-dark" href="#">SweeperFootball</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link text-light ml-4" href="#">Acceuil <span class="sr-only">Acceuil</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light ml-4" href="#">Actualité</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light ml-4" href="#">Catégorie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light ml-4" href="#">Qui-suis-je ?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light ml-4" href="#">Inscription</a>
+      </li>
+    </ul>
+    <form class="form-inline my-4 my-lg-0 offset-md-5">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
+    </form>
+  </div>
+</nav> 
+</header>
+   </div>
